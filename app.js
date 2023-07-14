86a00aeb6e7bc5c7b97549f91ac1a320fdfbe3b9
seo-seoungjun <tjtjdwns1860@naver.com>
2023-07-14 19:15:55 +0900

    bug:mongodb connection fail (#1)
    몽고디비 버전으로 인한 커넥션 실패 코드 수
    resolved: #1

--- app.js
+++ app.js
@@ -10,12 +10,28 @@ app.use('/', express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-const { MongoClient } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = env.DB_URL;
-const client = new MongoClient(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+
     const database = client.db('todolist');
     const loginCol = database.collection('login');
 
@@ -28,7 +44,7 @@ async function run() {
     });
 
     app.get('/register', (req, res) => {
-      res.render('login.ejs');
+      res.render('register.ejs');
     });
 
     app.use(
@@ -53,7 +69,7 @@ async function run() {
       const { id, pw } = req.body;
       loginCol.insertOne({ id, pw }, (err, result) => {
         if (err) throw err;
-        res.redirect('/main');
+        res.redirect('/');
       });
     });
 
@@ -109,6 +125,7 @@ async function run() {
       }
     );
   } finally {
+    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
